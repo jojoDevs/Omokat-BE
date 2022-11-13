fef13a9ab288b9f75df66377ea35df457e835f73
seulgiJ <qlenf1560@gmail.com>
2022-11-14 01:09:04 +0900

    feat. 로그인 구현

--- utils/encrypt.ts
+++ utils/encrypt.ts
@@ -6,16 +6,28 @@ const randomBytes = util.promisify(crypto.randomBytes);
 const pbkdf2 = util.promisify(crypto.pbkdf2);
 
 const createSalt = async () => {
-    const buf = await randomBytes(64);
-    return buf.toString("base64");
-}
-
-const createHashedPassword = async (password: string) => {
-    const salt = await createSalt();
-    const key = await pbkdf2(password, salt, 104904, 64, "sha512");
-    const hashedPassword = key.toString("base64");
-
-    return {hashedPassword, salt};
+  const buf = await randomBytes(64);
+  return buf.toString("base64");
 };
 
-export default createHashedPassword
+const createHashedPassword = async (password: string) => {
+  const salt = await createSalt();
+  const key = await pbkdf2(password, salt, 104904, 64, "sha512");
+  const hashedPassword = key.toString("base64");
+
+  return { hashedPassword, salt };
+};
+
+const verifyPassword = async (
+  password: string,
+  userPassword: string,
+  userSalt: string
+) => {
+  const key = await pbkdf2(password, userSalt, 104904, 64, "sha512");
+  const hashedPassword = key.toString("base64");
+
+  if (hashedPassword === userPassword) return true;
+  return false;
+};
+
+export { createHashedPassword, verifyPassword };
