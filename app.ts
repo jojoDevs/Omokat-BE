d6762f80278ad3d6e08bb1df2ac136e1014060ed
Haesol <osunpine@gmail.com>
2022-11-13 21:13:22 +0900

    feat. cors 추가

--- app.ts
+++ app.ts
@@ -1,19 +1,24 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
+
 import sequelize from "./utils/database";
 
 import userRoutes from "./routes/user";
 
 const app = express();
 
+app.use(cors());
+
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.use(userRoutes);
 
 sequelize
     .sync()
     .then(() => {
-        console.log('성공')
+        console.log('연결')
         app.listen(8080);
     })
     .catch((err) => console.log(err));
