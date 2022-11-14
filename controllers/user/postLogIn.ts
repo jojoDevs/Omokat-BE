49b92db91edac8cc10e8c0668690a02506644562
seulgiJ <qlenf1560@gmail.com>
2022-11-15 00:42:42 +0900

    feat. 로그인 수정

--- controllers/user/postLogIn.ts
+++ controllers/user/postLogIn.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import jwt, { Secret } from "jsonwebtoken";
 import * as dotenv from "dotenv";
-import Users from "../../models/users";
+import User from "../../models/user";
 import { verifyPassword } from "../../utils/encrypt";
 
 dotenv.config();
@@ -13,31 +13,33 @@ const postLogIn = async (req: Request, res: Response) => {
   const expiresIn = String(process.env.EXPIRES_IN);
   const issuer = String(process.env.ISSUER);
 
-  Users.findOne({ where: { id: id } })
+  let pic = "";
+  let token = "";
+
+  User.findOne({ id: id })
     .then(async (result) => {
       if (!result) {
         res.status(412).send({ message: "가입하지 않은 아이디예요." });
       } else {
-        const userPwd = result?.dataValues.password;
-        const userSalt = result?.dataValues.salt;
-        const name = result?.dataValues.name;
-        const pic = result?.dataValues.pic;
+        const userPwd = String(result.password);
+        const userSalt = String(result.salt);
+        const name = result.name;
+        pic = String(result.pic);
         const isVerify = await verifyPassword(password, userPwd, userSalt);
 
         if (isVerify) {
-          const token = jwt.sign({ id: id, name: name }, secretKey, {
+          token = jwt.sign({ id: id, name: name }, secretKey, {
             expiresIn: expiresIn,
             issuer: issuer,
           });
-          Users.update({ token: token }, { where: { id: id } })
-            .then(() => {
-              res.status(200).json({ token: token, pic: pic });
-            })
-            .catch((err) => console.log(err));
-        } else {
-          res.status(421).send({ message: "비밀번호가 달라요." });
+          result.token = token;
+
+          return result.save();
         }
       }
+    })
+    .then(() => {
+      res.status(200).json({ token: token, pic: pic });
     })
     .catch((err) => console.log(err));
 };
