5b65b0e3386b6bd9b0adb51f6482f0016122af23
seulgiJ <qlenf1560@gmail.com>
2022-11-15 00:42:53 +0900

    feat. 아이디 찾기 수정

--- controllers/user/postFindId.ts
+++ controllers/user/postFindId.ts
@@ -1,15 +1,16 @@
 import { Request, Response } from "express";
-import Users from "../../models/users";
+import User from "../../models/user";
 
 const postFindId = (req: Request, res: Response) => {
   const name = req.body.name;
 
-  Users.findOne({ where: { name: name } })
+  User.findOne({ name: name })
     .then((result) => {
+      console.log(result);
       if (!result) {
         res.status(432).send({ message: "가입하지 않은 닉네임이에요." });
       } else {
-        const id = result?.dataValues.id;
+        const id = result.id;
         res.status(200).send({ id: id });
       }
     })
